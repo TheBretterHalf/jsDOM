b7822996d44761c0151bd26d6ad469b7603116e3
Brett Stainken <brettstainken.student@careerdevs.com>
2018-12-12 01:50:04 +0000

    document.querySelector

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-const titles = document.getElementsByClassName('title');
+const wrap = document.querySelector('#wrapper');
+console.log(wrap);
 
-console.log(Array.isArray(titles));
-console.log(Array.isArray(Array.from(titles)));
-
-Array.from(titles).forEach(function(title){
-  console.log(title);
-});
-
-Array.from(titles).forEach(function(item){
-    console.log("item: "+item.innerText);
-});
-
-console.log("testing")
+const wrap2 = document.getElementById("wrapper").innerHTML;
+console.log(wrap);
