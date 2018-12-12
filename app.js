9d683d1b26226dd8f86621e1bba0ec9b079f0578
Brett Stainken <brettstainken.student@careerdevs.com>
2018-12-12 01:55:49 +0000

    querySelector('#book-list li:nth-child(2) .name')

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ const wrap = document.querySelector('#wrapper');
 console.log(wrap);
 
 const wrap2 = document.getElementById("wrapper").innerHTML;
-console.log(wrap);
+console.log(wrap2);
+
+const wrap3 = document.querySelector('#book-list li:nth-child(2) .name')
+console.log(wrap3)
