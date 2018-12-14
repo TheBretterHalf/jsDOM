72a426c52ba6144bd8f8118a2e87c52ccb0e03c5
Brett Stainken <brettstainken.student@careerdevs.com>
2018-12-14 00:44:48 +0000

    allbranches

--- app.js
+++
@@ -1,8 +0,0 @@
-const wrap = document.querySelector('#wrapper');
-console.log(wrap);
-
-const wrap2 = document.getElementById("wrapper").innerHTML;
-console.log(wrap2);
-
-const wrap3 = document.querySelector('#book-list li:nth-child(2) .name')
-console.log(wrap3)
